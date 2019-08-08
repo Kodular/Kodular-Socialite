6cdc00c67bc6543ac2b9cae12a8ccf69aed8615a
Sander Jochems <sanderjochems@hotmail.nl>
2019-08-09 00:03:49 +0200

    Create Provider.php

---
+++ Provider.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace SocialiteProviders\Kodular;
+
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+
+class Provider extends AbstractProvider
+{
+    /**
+     * Unique Provider Identifier.
+     */
+    const IDENTIFIER = 'KODULAR';
+
+    /**
+     * Get the authentication URL for the provider.
+     *
+     * @param string $state
+     * @return string
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase('https://auth.kodular.io/ska/authorize', $state);
+    }
+
+    /**
+     * Get the token URL for the provider.
+     *
+     * @return string
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://auth.kodular.io/ska/access_token';
+    }
+
+    /**
+     * Get the raw user for the given access token.
+     *
+     * @param string $token
+     * @return array
+     */
+    protected function getUserByToken($token)
+    {
+        // TODO: Implement getUserByToken() method.
+    }
+
+    /**
+     * Map the raw user array to a Socialite User instance.
+     *
+     * @param array $user
+     * @return \Laravel\Socialite\Two\User
+     */
+    protected function mapUserToObject(array $user)
+    {
+        // TODO: Implement mapUserToObject() method.
+    }
+}
